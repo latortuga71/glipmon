7b7be748085fa2cba47aab2a33d89a6cfa166e4f
christopher alonso <christopher.alonso@ibm.com>
2022-04-18 20:18:55 -0400

    first

---
+++ cmd/glipmon/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/progrium/macdriver/cocoa"
+)
+
+type ClipBoard struct {
+	sync.Mutex
+	ChangeCount int
+	Contents    string
+	Board       cocoa.NSPasteboard
+}
+
+func NewClipBoard() *ClipBoard {
+	board := cocoa.NSPasteboard_GeneralPasteboard()
+	changeCount := int(board.ChangeCount())
+	contents := board.StringForType(cocoa.NSPasteboardTypeString)
+	return &ClipBoard{
+		ChangeCount: changeCount,
+		Contents:    contents,
+		Board:       board,
+	}
+}
+func main() {
+	sigChannel := make(chan os.Signal, 1)
+	exit := make(chan interface{}, 1)
+	signal.Notify(sigChannel, syscall.SIGINT, syscall.SIGTERM)
+	board := NewClipBoard()
+	// check for clipboard changes
+	go func() {
+		for {
+			time.Sleep(time.Second * 1)
+			board.Lock()
+			changeCount := board.Board.ChangeCount()
+			if board.ChangeCount != int(changeCount) {
+				board.Contents = board.Board.StringForType(cocoa.NSPasteboardTypeString)
+				log.Printf("%s\n", board.Contents)
+				board.ChangeCount = int(changeCount)
+			}
+			board.Unlock()
+		}
+	}()
+	//listen for sigterms etc to exit program.
+	go func() {
+		test := <-sigChannel
+		exit <- test
+	}()
+	<-exit
+	fmt.Fprintf(os.Stderr, "Exiting...")
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/latortuga71/glipmon
+
+go 1.16
+
+require github.com/progrium/macdriver v0.2.0
